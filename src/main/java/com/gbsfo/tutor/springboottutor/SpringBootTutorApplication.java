e1bb22f78cc8d3234e5692928458685c745046c2
anfadiei <anfadiei@cisco.com>
2021-10-30 14:02:44 +0300

    Add files.

---
+++ src/main/java/com/gbsfo/tutor/springboottutor/SpringBootTutorApplication.java
@@ -0,0 +1,13 @@
+package com.gbsfo.tutor.springboottutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbsfo/tutor/springboottutor/SpringBootTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbsfo.tutor.springboottutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
